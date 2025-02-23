a2465a38778b9b7b9f7f25eb83afc6f416186aa4
Zveat <zveat123@gmail.com>
2025-02-24 02:42:21 +0500

    Update margin_calculator.py

--- margin_project/margin_calculator.py
+++ margin_project/margin_calculator.py
@@ -573,3 +573,13 @@ tab_margin, tab_logistics = st.tabs(["**Калькулятор маржинал�
 with tab_margin:
     # Запускаем сервис маржинальности
     run_margin_service()
+
+with tab_logistics:
+    st.markdown(
+        """
+        <div style="display:flex; justify-content:center; margin-top:20px;">
+            <iframe src="https://logistics-calculator-f0g7.onrender.com" height="600" width="1800" style="border:none;"></iframe>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
