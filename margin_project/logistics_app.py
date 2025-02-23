720858933606e4e19be0af796ab68e60c935636b
Zveat <zveat123@gmail.com>
2025-02-24 00:30:29 +0500

    Update logistics_app.py

--- margin_project/logistics_app.py
+++ margin_project/logistics_app.py
@@ -9,6 +9,8 @@ import locale
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='margin-top: 30px;'>Калькулятор логистики</h1>", unsafe_allow_html=True)
 
+def run_logistics_app():
+
 # Устанавливаем локаль
 try:
     locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
