de02ca513a688378af7682fd339396a0e6012501
Zveat <zveat123@gmail.com>
2025-02-25 00:01:25 +0500

    Update margin_calculator.py

--- margin_project/margin_calculator.py
+++ margin_project/margin_calculator.py
@@ -30,7 +30,7 @@ encoded_logo = base64.b64encode(data).decode()
 # Формируем HTML-блок: картинка + заголовок в одну строку
 html_block = f"""
 <div style="display:flex; align-items:center; margin-bottom:20px;">
-  <!-- width=200px - меняйте на нужный размер -->
+  <!-- width=400px - меняйте на нужный размер -->
   <img src="data:image/png;base64,{encoded_logo}" style="width:150px; margin-right:170px;" alt="Logo" />
   <h2 style="margin:0; font-size:30px;">
     Сервис расчета логистики и маржинальности
