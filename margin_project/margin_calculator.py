54524c26ce81fce7c5a3c0ec0c34dad91b0df256
Zveat <zveat123@gmail.com>
2025-02-25 00:16:09 +0500

    Update margin_calculator.py

--- margin_project/margin_calculator.py
+++ margin_project/margin_calculator.py
@@ -31,7 +31,7 @@ html_block = f"""
   <!-- width=200px - меняйте на нужный размер -->
   <img src="data:image/png;base64,{encoded_logo}" style="width:200px; margin-right:170px;" alt="Logo" />
   <h2 style="margin:0; font-size:25px;">
-    Сервис расчета логистики и ㅤㅤㅤㅤмаржинальности
+  <span style="color:blue;">Сервис расчета логистики и ㅤㅤㅤㅤмаржинальности</span>
   </h2>
 </div>
 """
