ccf492cc77c2c73dac955aa89c0b110e78dbcd20
Zveat <zveat123@gmail.com>
2025-02-24 23:00:35 +0500

    Update margin_calculator.py

--- margin_project/margin_calculator.py
+++ margin_project/margin_calculator.py
@@ -15,7 +15,7 @@ st.set_page_config(layout="wide")
 
 # Заголовок приложения (можно убрать, если не нужен глобальный заголовок)
 st.markdown(
-    "<h4 style='text-align:center; font-weight:bold;'>Сервис расчета логистики и маржинальности</h4>",
+    "<h4 style='text-align:center; font-weight:bold;'>Сервис расчета логистики и маржинальности</h3>",
     unsafe_allow_html=True
 )
 
