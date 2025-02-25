cc87485bc04be752b37ce6517cd180b53d880a71
Zveat <zveat123@gmail.com>
2025-02-25 20:26:22 +0500

    Update margin_calculator.py

--- margin_project/margin_calculator.py
+++ margin_project/margin_calculator.py
@@ -108,7 +108,7 @@ st.markdown(html_block, unsafe_allow_html=True)
 st.markdown("""
 <style>
     .main .block-container {
-        max-width: 1300px;
+        max-width: 1600px;
         padding: 1rem;
     }
 </style>
