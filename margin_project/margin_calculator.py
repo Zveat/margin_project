133f48796a6dd099d4bcb9aa53aa419be6bccf21
Zveat <zveat123@gmail.com>
2025-02-28 20:20:23 +0500

    Update margin_calculator.py

--- margin_project/margin_calculator.py
+++ margin_project/margin_calculator.py
@@ -158,7 +158,7 @@ def run_logistics_service():
         }
         /* Стили для кнопок */
         div.stButton > button {
-             background-color: #2F4F4F;
+             background-color: #1E90FF;
              color: #FFFFFF;
              border: none;
              border-radius: 5px;
